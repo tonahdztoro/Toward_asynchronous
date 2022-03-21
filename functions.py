12da2a7ba20bb8d337d7111cd51f3ecec3f276b1
tonahdztoro <63756460+tonahdztoro@users.noreply.github.com>
2022-03-21 11:59:45 -0600

    Update functions.py
    added arxiv link

--- functions.py
+++ functions.py
@@ -7,7 +7,7 @@ Code for the paper
 "Toward asynchronous EEG-based BCI: Detecting imagined words segments in continuous EEG signals"
 published on the journal "Biomedical Signal Processing and Control"
 
-arXiv: 
+arXiv:2105.04294
 doi: 10.1016/j.bspc.2020.102351
 
 
